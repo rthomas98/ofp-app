91a952542a5df1058dd646596bec442399b23c57
Rob Thomas <robt84@gmail.com>
2023-07-16 09:45:37 -0500

    added new icons to the bottom nav bar

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "expo-router";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from ".";
-import { Ionicons, Feather } from '@expo/vector-icons'  // https://icons.expo.fyi/
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { FontAwesome } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
 import donationList from "./donationList";
 import myFundraising from "./myFundraising";
 import inbox from "./inbox";
@@ -19,28 +21,30 @@ export default function HomeLayout() {
   
               if (route.name === 'Home') {
                 iconName = focused
-                  ? 'ios-information-circle'
-                  : 'ios-information-circle-outline';
+                  ? 'home-sharp'
+                  : 'home-outline';
               } else if (route.name === 'Donations') {
-                iconName = focused ? 'ios-list' : 'ios-list-outline';
+                iconName = focused ? 'cash' : 'cash-outline';
               } else if (route.name === 'Fundraising') {
-                iconName = focused ? 'ios-list' : 'ios-list-outline';
+                iconName = focused ? 'bar-chart' : 'bar-chart-outline';
               } else if (route.name === 'Inbox') {
-                iconName = focused ? 'ios-list' : 'ios-list-outline';
+                iconName = focused ? 'mail' : 'mail-outline';
               } else if (route.name === 'Profile') {
-                iconName = focused ? 'ios-list' : 'ios-list-outline';
+                iconName = focused ? 'body' : 'body-outline';
               }
 
   
               // You can return any component that you like here!
               return <Ionicons name={iconName} size={size} color={color} />;
             },
-            tabBarActiveTintColor: 'tomato',
-            tabBarInactiveTintColor: 'gray',
+            tabBarActiveTintColor: '#FF1843',
+            tabBarInactiveTintColor: '#2C3A4B',
             tabBarShowLabel: true,
           })}
         >
-            <Tab.Screen name="Home" component={HomeScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} options={{
+                headerTintColor: '#FF1843',
+            }} />
             <Tab.Screen name="Donations" component={donationList} />
             <Tab.Screen name="Fundraising" component={myFundraising} />
             <Tab.Screen name="Inbox" component={inbox} />
