2c720dff588d6f8f17dc5d9ae3b919e5336ce7db
Rob Thomas <robt84@gmail.com>
2023-08-08 10:48:24 -0500

    updates

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { Redirect } from "expo-router";
 
 export default function Page() {
-    return <Redirect href={'/home/donationList'} />;
+    return <Redirect href={'/auth/'} />;
 
 }
 
